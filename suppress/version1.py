64c8fd20cc773b3bc735263d63c34d370496d555
Ishikawa2 <satoru.ishikawa@cross-compass.com>
2022-12-16 15:51:12 +0900

    add suppress

---
+++ suppress/version1.py
@@ -0,0 +1,24 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def suppress(errortype):
+    try:
+        yield
+    except errortype as e:
+        pass
+    
+
+with suppress(NameError):
+    print("Hi!")
+    print("It's nice to meet you,", name)
+    print("Goodbye!")
+x = 0
+with suppress(ValueError):
+    x = int('hello')
+
+print(x)
+with suppress(TypeError):
+    print("Hi!")
+    print("It's nice to meet you,", name)
+    print("Goodbye!")
---
+++ suppress/version4.py
@@ -0,0 +1,44 @@
+import sys
+import traceback
+import contextlib
+
+class suppress(contextlib.ContextDecorator):
+    def __init__(self, *args):
+        self.errortype = args
+        self.traceback = None
+        self.exception = None
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type not in self.errortype:
+            # raise exc_type from None
+            return False
+        self.traceback = exc_tb
+        self.exception = exc_val
+        return True
+
+
+with suppress(NameError):
+    print("Hi!")
+    print("It's nice to meet you,", name)
+    print("Goodbye!")
+x = 0
+with suppress(ValueError):
+    x = int('hello')
+print(x)
+with suppress(ValueError, TypeError):
+    x = int('hello')
+with suppress(ValueError, TypeError):
+    x = int(None)
+
+with suppress(ValueError, TypeError) as context:
+    x = int('hello')
+print(context.exception)
+print(context.traceback)
+
+with suppress(TypeError):
+    print("Hi!")
+    print("It's nice to meet you,", name)
+    print("Goodbye!")
